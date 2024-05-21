b2070ab04ab4643288682996aa76dbe74c434849
thePiyushPriyadarshi <piyushjha.ktr@gmail.com>
2024-05-21 18:28:35 +0530

    fixed form submitting issue

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -7,6 +7,7 @@
   function handleSubmit(event) {
     event.preventDefault();
     isLoading = true; 
+    email = email.toLowerCase()
     fetch("https://golang-production-ebec.up.railway.app/login", {
       method: "POST",
       body: JSON.stringify({
@@ -44,8 +45,7 @@
         class="bg-gradient-to-r font-semibold from-[#0796FF] to-[#91F6FF] bg-clip-text text-transparent"
         >LENS CORPORATION</span
       >
-    </h3>
-    <form class="flex flex-col items-center gap-6" on:submit={handleSubmit}>
+    </h3> 
       <label class="flex flex-col gap-1">
         <p class="text-left">Email <sup class="text-rose-500">*</sup></p>
         <input
@@ -68,11 +68,11 @@
       </label>
       <p class="text-left text-rose-500">{error}</p>
       <button
+        on:click={handleSubmit}
         disabled={isLoading}
         type="submit"
         class="border-white disabled:bg-gray-400 hover:bg-white hover:text-black transition-all duration-200 border px-6 py-2 w-[320px] rounded-lg"
         >Login</button
-      >
-    </form>
+      > 
   </div>
 </div>
--- src/routes/register/+page.svelte
+++ src/routes/register/+page.svelte
@@ -1,35 +1,37 @@
 <script>
- import {goto} from "$app/navigation"
+  import { goto } from "$app/navigation";
   let email = "";
   let password = "";
-  let name = ""
-  let error = ""
+  let name = "";
+  let error = "";
   let isLoading = false;
   function handleSubmit(event) {
     event.preventDefault();
-     isLoading = true;  
-	fetch("https://golang-production-ebec.up.railway.app/register",{
-		method: "POST",
-		body: JSON.stringify({
-			email,
-      name,
-			password
-		}),
-		headers : {
-			"Content-Type": "application/json; charset=UTF-8",
-		}
-	}).then((response) => response.json())
-	.then((json)=> {alert(json.message)
-    email = "";
-    password = "";
-    name = "";
-		isLoading=false
-		if(json.status) goto("/login")
-	})
-	.catch((error) => {
-    console.error('Error:', error);
-});
- 
+    isLoading = true;
+    email = email.toLowerCase()
+    fetch("https://golang-production-ebec.up.railway.app/register", {
+      method: "POST",
+      body: JSON.stringify({
+        email,
+        name,
+        password,
+      }),
+      headers: {
+        "Content-Type": "application/json; charset=UTF-8",
+      },
+    })
+      .then((response) => response.json())
+      .then((json) => {
+        alert(json.message);
+        email = "";
+        password = "";
+        name = "";
+        isLoading = false;
+        if (json.status) goto("/login");
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
   }
 </script>
 
@@ -43,13 +45,11 @@
         class="bg-gradient-to-r font-semibold from-[#0796FF] to-[#91F6FF] bg-clip-text text-transparent"
         >LENS CORPORATION</span
       >
-    </h3>
-    <form class="flex flex-col items-center gap-6" on:submit={handleSubmit}>
-      
+    </h3> 
       <label class="flex flex-col gap-1">
         <p class="text-left">Full Name <sup class="text-rose-500">*</sup></p>
         <input
-		required
+          required
           class="bg-gray-800 w-[320px] text-white rounded-lg py-2 px-4"
           type="text"
           placeholder="Enter your name"
@@ -59,7 +59,7 @@
       <label class="flex flex-col gap-1">
         <p class="text-left">Email <sup class="text-rose-500">*</sup></p>
         <input
-		required
+          required
           class="bg-gray-800 w-[320px] text-white rounded-lg py-2 px-4"
           type="email"
           placeholder="Enter your email"
@@ -69,20 +69,20 @@
       <label class="flex flex-col gap-1">
         <p class="text-left">Password <sup class="text-rose-500">*</sup></p>
         <input
-		required
+          required
           class="bg-gray-800 w-[320px] text-white rounded-lg py-2 px-4"
           type="password"
           placeholder="Enter your password"
           bind:value={password}
         />
       </label>
-	  <p class="text-left text-rose-500">{error}</p>
+      <p class="text-left text-rose-500">{error}</p>
       <button
-	  disabled={isLoading}
-	  type="submit"
+        on:click={handleSubmit}
+        disabled={isLoading}
+        type="submit"
         class="border-white disabled:bg-gray-400 hover:bg-white hover:text-black transition-all duration-200 border px-6 py-2 w-[320px] rounded-lg"
         >Register</button
-      >
-    </form>
+      > 
   </div>
 </div>
